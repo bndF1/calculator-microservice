06bc84ac6b91e0910e5c05212c5572817f159fba
bndF1 <jesbegar@gmail.com>
2019-09-22 15:18:08 +0200

    Added missing implements on DTOs

--- src/main/java/com/bndf1/test/calculator/domain/dto/OperandDTO.java
+++ src/main/java/com/bndf1/test/calculator/domain/dto/OperandDTO.java
@@ -3,11 +3,13 @@ package com.bndf1.test.calculator.domain.dto;
 import lombok.Builder;
 import lombok.Value;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Value
 @Builder(toBuilder = true)
-public class OperandDTO {
+public class OperandDTO implements Serializable {
+  private static final long serialVersionUID = 3378723997062490515L;
   BigDecimal firstOperand;
   BigDecimal secondOperand;
 }
